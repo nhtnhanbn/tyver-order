5e406b7b96e95922712e44c99da2cafa7ca29212
nhtnhanbn <nhtnhanbn@gmail.com>
2026-02-05 10:08:06 +1100

    Fix Vite configuration for multi-page app

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/order/"
+	base: "/order/",
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				configurations: resolve(__dirname, "configurations/index.html"),
+				vic2022BtlNorthEasternMetropolitan: resolve(__dirname, "vic/2022/btl/north-eastern-metropolitan/index.html")
+			}
+		}
+	}
 });
